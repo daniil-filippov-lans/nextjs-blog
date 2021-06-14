8c22baf9fc88738abc47855f6b0b474772481424
danil_lans <danimen0001@gmail.com>
2021-06-14 23:22:51 +0300

    добавил ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,22 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
 	const allPostsData = getSortedPostsData();
 	return { props: { allPostsData } };
-}
+};
 
-export default function Home({ allPostsData }) {
+export default function Home({
+	allPostsData,
+}: {
+	allPostsData: {
+		date: string;
+		title: string;
+		id: string;
+	}[];
+}) {
 	return (
 		<Layout home>
 			<Head>

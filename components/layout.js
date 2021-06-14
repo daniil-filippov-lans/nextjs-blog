a7bf288e4435b31fdb8ce3ae78195214fb91ee88
danil_lans <danimen0001@gmail.com>
2021-06-14 13:39:44 +0300

    готово к деплою

---
+++ components/layout.js
@@ -0,0 +1,69 @@
+import css from './layout.module.css';
+import Image from 'next/image';
+import Head from 'next/head';
+import utilsCss from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Danya';
+
+export const siteTitle = 'Next.js Sample Website';
+
+export default function Layout({ children, home }) {
+	return (
+		<div className={css.container}>
+			<Head>
+				<link rel="icon" href="/public/favicon.ico" />
+				<meta
+					name="description"
+					content="Learn how to build a personal website using Next.js"
+				/>
+				<meta property="og:title" content={siteTitle} />
+				<meta name="twitter:card" content="summary_large_image" />
+			</Head>
+			<header className={css.header}>
+				{home ? (
+					<>
+						{' '}
+						<Image
+							priority
+							src="/images/profile.jpg"
+							className={utilsCss.borderCircle}
+							height={144}
+							width={144}
+							alt={name}
+						/>{' '}
+						<h1 className={utilsCss.heading2Xl}>{name}</h1>{' '}
+					</>
+				) : (
+					<>
+						<Link href="/">
+							<a>
+								<Image
+									priority
+									src="/images/profile.jpg"
+									className={utilsCss.borderCircle}
+									height={108}
+									width={108}
+									alt={name}
+								/>
+							</a>
+						</Link>
+						<h2 className={utilsCss.headingLg}>
+							<Link href="/">
+								<a className={utilsCss.colorInherit}>{name}</a>
+							</Link>
+						</h2>
+					</>
+				)}
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className={css.backToHome}>
+					<Link href="/">
+						<a>← Back to home</a>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
+}
